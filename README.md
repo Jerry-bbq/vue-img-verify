ff28618a002947ea2344c7bc17a9374f995fe429
lujing <lujing@ACA86FCE.ipt.aol.com>
2018-04-15 17:02:24 +0800

    图形验证码组件

---
+++ README.md
@@ -0,0 +1,28 @@
+## 基于vue的前端图形验证码
+
+## 效果图
+
+![效果图](./img/img.png)
+
+## 说明
+
+* 4位数字加大写字母的验证码
+
+* 点击可以实现图片的变化
+
+## 使用canvas
+
+## 使用
+
+* 引入组件 img-verify
+
+* 接受子组件img-verify 传递的参数
+
+
+	  <ImgVerify @printCanvas="imgCode"/>
+
+	  methods: {
+        mgCode(code){
+          console.log(code)
+        }
+    },
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h3>前端图片验证码：</h3>
+    <ImgVerify @printCanvas="imgCode"/>
+    <fieldset>
+      <legend>说明：</legend>
+      <ul>
+        <li>使用canvas实现图片验证码</li>
+        <li>4位数字与大写字母随机显示</li>
+        <li>5条干扰线</li>
+        <li>40个干扰小点</li>
+      </ul>
+    </fieldset>
+  </div>
+</template>
+
+<script>
+import ImgVerify from './components/img-verify'
+
+export default {
+  name: 'App',
+  methods: {
+    imgCode(code){
+      console.log(code)
+    }
+  },
+  components: {
+    ImgVerify
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
